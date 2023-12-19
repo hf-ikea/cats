e749f3749b9700c6abce88110cdb8a29f35fe2e2
Giovanni Forma <bluehairguyiscool@gmail.com>
2023-12-18 18:47:49 -0800

    clean

--- CRC.cs
+++ CRC.cs
@@ -6,7 +6,7 @@ namespace CATS
         // https://gist.github.com/bryc/79d1a62304773285317191f1ae5aa5b8
         // uses a reflected 0x1021 polynomial (0x8408)
         // inverted (xor with 0xFFFF)
-        public ushort CRCRemainder(byte[] data)
+        public static ushort CRCRemainder(byte[] data)
         {
             uint t = 0;
             uint crc = 0xFFFF;
@@ -14,7 +14,7 @@ namespace CATS
             {
                 crc &= 0xFFFF;
                 t = crc ^ data[i];
-                t = (t ^ t << 4) & 0xFF;
+                t = (t ^ (t << 4)) & 0xFF;
                 crc = (crc >> 8) ^ (t << 8) ^ (t >> 4) ^ (t << 3);
             }
             return (ushort)~crc;
--- Program.cs
+++ Program.cs
@@ -2,5 +2,4 @@ using CATS;
 
 byte[] test = "123456789"u8.ToArray();
 
-CRC c = new();
-Console.WriteLine(c.CRCRemainder(test));
+Console.WriteLine(CRC.CRCRemainder(test));
