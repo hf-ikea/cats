f17b8678026fe6f38ac9c4d42f265e6c28dcfa48
Giovanni Forma <bluehairguyiscool@gmail.com>
2023-12-18 18:35:10 -0800

    wokring

---
+++ CRC.cs
@@ -0,0 +1,24 @@
+namespace CATS
+{
+    public class CRC
+    {
+        // 16 bit ibm sdlc crc
+        // https://gist.github.com/bryc/79d1a62304773285317191f1ae5aa5b8
+
+        // uses a reflected 0x1021 polynomial (0x8408)
+        // inverted (xor with 0xFFFF)
+        public ushort CRCRemainder(byte[] data)
+        {
+            uint t = 0;
+            uint crc = 0xFFFF;
+            for(uint i = 0; i < data.Length; i++)
+            {
+                crc &= 0xFFFF;
+                t = crc ^ data[i];
+                t = (t ^ t << 4) & 0xFF;
+                crc = (crc >> 8) ^ (t << 8) ^ (t >> 4) ^ (t << 3);
+            }
+            return (ushort)~crc;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,22 +1,6 @@
 using CATS;
 
-NodeInfo i = new();
-List<Variable> list = new()
-{
-    new Variable(VariableType.TrancieverTemp, -8),
-    new Variable(VariableType.Uptime, uint.MaxValue),
-    new Variable(VariableType.AntennaGain, 4),
-    new Variable(VariableType.SWID, 68),
-    new Variable(VariableType.HWID, 7888),
-    new Variable(VariableType.AntennaHeight, 8),
-    new Variable(VariableType.Voltage, 4)
-};
+byte[] test = "123456789"u8.ToArray();
 
-i.GetBitmap(list);
-i.PushVariableList(list);
-
-foreach(Variable v in i.GetVariableList())
-{
-    Console.WriteLine(v.type.ToString());
-    Console.WriteLine(v.value);
-}
+CRC c = new();
+Console.WriteLine(c.CRCRemainder(test));
