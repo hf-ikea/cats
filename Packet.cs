e121777ff73d4d13abf74be1e6e29c51150c5c80
Giovanni Forma <bluehairguyiscool@gmail.com>
2023-12-24 15:53:26 -0800

    guh

--- Packet.cs
+++ Packet.cs
@@ -2,43 +2,45 @@ namespace CATS
 {
     public class Packet
     {
-        public List<byte> byteList = new(8189); // NOT encoded (no CRC, whiten, ldpc, interleave, header) this is just the bytes from the whiskers concat together LENGTH: 2 less than 8189 to accomodate the CRC check
-        public bool hasIdentification = false;
-        public bool hasTimestamp = false;
-        public bool hasGPS = false;
-        public bool hasRoute = false;
-
-        public Packet()
-        {
-
-        }
         // after crc
-        public byte[] SemiEncode()
+        public static byte[] SemiEncode(List<Whisker> whiskerList)
         {
-            ushort crc = CRC.GetChecksum(byteList);
-            byte[] encoded = new byte[byteList.Count + 2];
-            if(encoded.Length > 8191) throw new Exception("Packet is too long!");
-            Array.Copy(BitConverter.GetBytes(crc), 0, encoded, byteList.Count, 2);
+            List<byte> bytes = new(8189);
+            foreach(Whisker w in whiskerList)
+            {
+                bytes.Add((byte)w.type);
+                bytes.AddRange(w.data);
+            }
+
+            if(bytes.Count + 2 > 8191) throw new Exception("Packet is too long!");
+
+            byte[] encoded = new byte[bytes.Count + 2];
+            Array.Copy(BitConverter.GetBytes(CRC.GetChecksum(bytes)), 0, encoded, bytes.Count, 2);
             
             return encoded;
         }
 
-        // encodeds packet without header
-        public byte[] FullEncode()
+        // encodes packet without header
+        public static byte[] FullEncode(List<Whisker> whiskerList)
         {
-            byte[] packet = SemiEncode();
+            byte[] packet = SemiEncode(whiskerList);
             packet = Whitener.Whiten(packet);
             packet = LDPC.Encode(packet);
 
             return Interleaver.Interleave(packet);
         }
 
-        public List<Whisker> SemiDecode(byte[] packet)
+        public static List<Whisker> SemiDecode(byte[] packet)
         {
             byte[] data = packet[..^3];
             ushort expectedCRC = CRC.GetChecksum(data);
             ushort recievedCRC = BitConverter.ToUInt16(packet.AsSpan()[^2..]);
             if(expectedCRC != recievedCRC) throw new Exception("CRC does not match!");
+
+            bool hasIdentification = false;
+            bool hasTimestamp = false;
+            bool hasGPS = false;
+            bool hasRoute = false;
 
             List<Whisker> whiskers = new();
             foreach(Whisker w in Whisker.GetWhiskers(data))
@@ -71,12 +73,24 @@ namespace CATS
         }
 
         // decodes entire packet without header
-        public List<Whisker> FullDecode(byte[] data)
+        public static List<Whisker> FullDecode(byte[] data)
         {
             data = Interleaver.Deinterleave(data);
             data = LDPC.Decode(data);
             data = Whitener.Whiten(data);
             return SemiDecode(data);
+        }
+
+        // split a comment into several whiskers if it is too long
+        public static List<Whisker> SplitComment(string comment)
+        {
+            List<Whisker> list = new();
+            for(int i = 0; i < comment.Length; i += 255)
+            {
+                list.Add(new Whisker(WhiskerType.Comment, new Comment(comment[i..(i + 255)]).Encode()));
+            }
+
+            return list;
         }
     }
 }
